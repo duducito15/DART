20ea19e1657f78a11a1ed23a29319245dd920112
Eduardo Chavez <echavezv@utea.edu.pe>
2023-08-18 07:33:28 -0500

    Crear codigo

--- string.dart
+++ string.dart
@@ -25,10 +25,29 @@ asi se configura
   print(texto);
   print(texto1); */
 
-  String nombre = "    Juan Gabriel Lopez    ";
+  String nombre = "Juan Gabriel Lopez";
   print(nombre.isEmpty); //trato el string como un booleano
   print(nombre.length); //trato el string como un entero
   print(nombre.toUpperCase());
   print(nombre.toLowerCase());
   print(nombre.trim());
+  print(nombre.compareTo("juan gabriel lopez"));
+  print(nombre.replaceAll("a", "e"));
+  print(nombre.substring(4, 10));
+  print(nombre.contains("juan"));
+  print(nombre.startsWith("A"));
+  print(nombre.endsWith("z"));
+
+  String dni = "40160915";
+  String name = "Eduardo";
+  String lastName = "Chavez Vasquez";
+
+  /*
+  1. Crear un codigo personalizado de usuario
+  2. Primeras letras del nombre, las dos ultimas letras del apellido
+  y los 4 ultimos digitos del DNI.
+  ******** Edez0815 *******
+
+  
+  */
 }
