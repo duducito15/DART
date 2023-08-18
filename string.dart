35ea25d056892bd4c750243eda04584d74413ccc
Eduardo Chavez <echavezv@utea.edu.pe>
2023-08-18 06:56:15 -0500

    String  - metodos

--- string.dart
+++ string.dart
@@ -1,6 +1,6 @@
 void main(List<String> args) {
   //Colecciones
-  String texto = 'Mi texto';
+/*   String texto = 'Mi texto';
   String textoMultilinea = '''Mi texto
 es
 multilinea
@@ -15,5 +15,20 @@ asi se configura
 
   int edad = 20;
   String nombre = "Juan";
-  print("Mi nombre es $nombre y tengo $edad años");
+  print("Mi nombre es $nombre y tengo $edad años"); */
+
+/*   int numero = 100;
+  double precio = 15.50;
+  String texto = numero.toString();
+  String texto1 = precio.toString();
+
+  print(texto);
+  print(texto1); */
+
+  String nombre = "    Juan Gabriel Lopez    ";
+  print(nombre.isEmpty); //trato el string como un booleano
+  print(nombre.length); //trato el string como un entero
+  print(nombre.toUpperCase());
+  print(nombre.toLowerCase());
+  print(nombre.trim());
 }
